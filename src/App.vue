e54855c0ddf9c862714622b8bf2f97181b087d81
Victor Morais <victormoraislg@gmail.com>
2022-11-09 15:01:39 -0300

    Adding router to separate views

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,7 @@
       <SideBar @onChangeThemeColor="changeThemeColor"/>
     </div>
     <div class="column is-10 content">
-      <TaskForm @onSaveTask="saveTask"/>
-      <div class="list">
-        <TaskItem v-for="(task, index) in tasks" :key="index" :task="task"/>
-        <BoxItem v-if="taskListIsEmpty">
-            Apparently you haven't started any task yet.
-        </BoxItem>
-      </div>
+      <RouterView></RouterView>
     </div>
   </main>
 </template>
@@ -18,34 +12,18 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import SideBar from './components/SideBar.vue';
-import TaskForm from './components/Task/Form.vue';
-import TaskItem from './components/Task/Task.vue';
-import ITask from './interfaces/ITask';
-import BoxItem from './components/Box.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     SideBar,
-    TaskForm,
-    TaskItem,
-    BoxItem
   },
   data() {
     return {
-      tasks: [] as ITask[],
       darkModeActivated: false,
     };
   },
-  computed: {
-    taskListIsEmpty() : boolean {
-      return this.tasks.length === 0;
-    },
-  },
   methods: {
-    saveTask (task: ITask) {
-      this.tasks.push(task);
-    },
     changeThemeColor(darkModeActivated: boolean) {
       this.darkModeActivated = darkModeActivated;
     }
@@ -71,5 +49,4 @@ main.dark-mode {
 .content {
   background-color: var(--primary-bg);
 }
-
 </style>
