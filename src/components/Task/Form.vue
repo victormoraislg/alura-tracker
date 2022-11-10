a1321684f889848f0efa46553d314e54abea8645
Victor Morais <victormoraislg@gmail.com>
2022-11-10 10:38:58 -0300

    Saving projects using states

--- src/components/Task/Form.vue
+++ src/components/Task/Form.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="box taskForm">
     <div class="columns">
-      <div class="column is-8" role="Form" aria-label="Form to create new task">
+      <div class="column is-6" role="Form" aria-label="Form to create new task">
         <input type="text" class="input" placeholder="Wich task do you want to start?" v-model="taskDescription">
       </div>
-      <div class="column">
+      <div class="column is-2">
+        <div class="select">
+          <select v-model="projectId">
+            <option value="">Select the project</option>
+            <option :value="project.id" v-for="project in projects" :key="project.id"> {{ project.name }} </option>
+          </select>
+        </div>
+      </div>
+      <div class="column is-4">
         <TaskTimer @onStop="archiveTask"/>
       </div>
     </div>
@@ -13,7 +21,10 @@
 
 <script lang="ts">
 
+import { key } from '@/store';
+import { computed } from '@vue/reactivity';
 import { defineComponent } from 'vue';
+import { useStore } from 'vuex';
 import TaskTimer from './Timer.vue';
 
 export default defineComponent({
@@ -27,16 +38,25 @@ export default defineComponent({
   data() {
     return {
       taskDescription: '',
+      projectId: '',
     };
   },
   methods: {
     archiveTask(timeInSeconds: number) : void {
       this.$emit('onSaveTask', {
         durationInSeconds: timeInSeconds,
-        description: this.taskDescription
+        description: this.taskDescription,
+        project: this.projects.find(proj => proj.id == this.projectId)
       });
       this.taskDescription = '';
     },
+  },
+  setup() {
+    const store = useStore(key);
+    
+    return {
+      projects: computed(() => store.state.projects)
+    }
   }
 });
 
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,13 +1,63 @@
 <template>
-  <h1>Projects list</h1>
+  <section class="projects">
+    <h1 class="title">Projects list</h1>
+    <form @submit.prevent="saveProject">
+      <div class="field">
+        <label for="projectName" class="label">Project name</label>
+        <input type="text" id="projectName" class="input" v-model="projectName">
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Save</button>
+      </div>
+    </form>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>NAME</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects" :key="project.id">
+          <td> {{ project.id }} </td>
+          <td> {{ project.name }} </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { useStore } from '@/store';
+import { computed, defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'ProjectsView'
+  name: 'ProjectsView',
+  data() {
+    return {
+      projectName: ''
+    };
+  },
+  methods: {
+    saveProject() {
+      this.store.commit('ADD_PROJECT', this.projectName);
+      this.projectName = '';
+    }
+  },
+  setup() {
+    const store = useStore();
+
+    return {
+      store,
+      projects: computed(() => store.state.projects)
+    }
+  }
 });
 
 </script>
+
+<style scoped>
+.projects {
+  padding: 1.5rem;
+}
+</style>
