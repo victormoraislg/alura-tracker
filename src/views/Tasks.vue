84f6d1a5363356d44665bbc14dfc4d2abc8dd61f
Victor Morais <victormoraislg@gmail.com>
2022-11-15 20:31:38 -0300

    Making modal a component

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -13,25 +13,22 @@
       Apparently you haven't started any task yet.
     </BoxItem>
     <TaskItem v-for="(task, index) in tasks" :key="index" :task="task" @onTaskSelected="selectTask"/>
-    <div class="modal" :class="{'is-active': selectedTask}" v-if="selectedTask">
-      <div class="modal-background"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Edit task</p>
-          <button class="delete" aria-label="close" @click="closeModal"></button>
-        </header>
-        <section class="modal-card-body">
-          <div class="field">
-        <label for="description" class="label">Description</label>
-        <input type="text" id="description" class="input" v-model="selectedTask.description">
-      </div>
-        </section>
-        <footer class="modal-card-foot">
-          <button class="button is-success" @click="editTask">Save</button>
-          <button class="button" @click="closeModal">Cancel</button>
-        </footer>
-      </div>
-    </div>
+    <TaskModal :show="selectedTask != null">
+      <template v-slot:header>
+        <p class="modal-card-title">Edit task</p>
+        <button class="delete" aria-label="close" @click="closeModal"></button>
+      </template>
+      <template v-slot:body>
+        <div class="field">
+          <label for="description" class="label">Description</label>
+          <input type="text" id="description" class="input" v-model="selectedTask.description">
+        </div>
+      </template>
+      <template v-slot:footer>
+        <button class="button is-success" @click="editTask">Save</button>
+        <button class="button" @click="closeModal">Cancel</button>
+      </template>
+    </TaskModal>
   </div>
 </template>
 
@@ -44,13 +41,15 @@ import BoxItem from '../components/Box.vue';
 import { useStore } from '@/store';
 import { CREATE_TASK, EDIT_TASK, REQUEST_PROJECTS, REQUEST_TASKS } from '@/store/actions';
 import { computed } from '@vue/reactivity';
+import TaskModal from '@/components/Modal.vue';
 
 export default defineComponent({
   name: 'TasksView',
   components: {
     TaskForm,
     TaskItem,
-    BoxItem
+    BoxItem,
+    TaskModal
   },
   data() {
     return {
